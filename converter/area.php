3384379142d8623b17fb971f292e45b38ff05eed
chapmang <geoff.chapman@mac.com>
2014-03-19 23:42:08 +0000

    Start work on area converter

---
+++ converter/area.php
@@ -0,0 +1,37 @@
+<?php
+namespace PHPConverter\Converter {
+
+	use PHPConverter\Core\Configuration as Configuration;
+
+	/**
+	* 
+	*/
+	class Area extends Converter {
+		
+		protected $_constants = array();
+
+		public function __construct() {
+
+			$this->_constants = Configuration::get('area');
+		}
+
+		protected function areaConversion($toUnit) {
+
+			if (array_key_exists($this->_fromUnit, $this->_constants)) {
+				$fromUnit = $this->_constants[$this->_fromUnit];
+				$toUnit = $this->_constants[$toUnit];
+				return ($this->_value * $fromUnit) / $toUnit;
+			} else {
+				throw new \Exception("{toUnit} is not a supported unit", 1);
+				
+			}
+
+		}
+
+		public function __call($toUnit, $parameters){
+
+			return call_user_func(array($this, 'areaConversion'), $toUnit);
+		}
+
+	}
+}
