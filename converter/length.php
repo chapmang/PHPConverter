b84c67de54b230f94d187fd120bfe7a46e2d5e17
chapmang <geoff.chapman@mac.com>
2014-03-19 23:41:37 +0000

    Establish core structure measurement conversion

--- converter/length.php
+++ converter/length.php
@@ -4,30 +4,82 @@ namespace PHPConverter\Converter {
 	use PHPConverter\Core\Configuration as Configuration;
 
 	/**
-	* 
-	*/
+	 * Length
+	 * Converter for converting between units of length
+	 * @author Geoff Chapman <geoff.chapman@mc.com>
+	 * @version 1.0
+	 * @package PHPConverter\Converter 
+	 */
 	class Length extends Converter {
 		
-		protected $_constants = array();
+		/**
+		 * Units within this measurement
+		 * @var array
+		 */
+		protected $_units = array();
 
+		/**
+		 * Aliases used to refer to units within this measurement
+		 * @var array 
+		 */ 
+		protected $_aliases = array();
+
+		/**
+		 * Retrieve the preregistered units and aliases for this measurement
+		 * @return void
+		 */
 		public function __construct() {
 
-			$this->_constants = Configuration::get('length');
+			$this->_units = Configuration::get('length.units');
+			$this->_aliases = Configuration::get('length.aliases');
 		}
 
+		/**
+		 * Convert measurement to the given unit
+		 * @param string $toUnit Unit to be converted to
+		 * @return float 		 The measurement in the desired unit
+		 */
 		protected function lengthConversion($toUnit) {
 
-			if (array_key_exists($this->_fromUnit, $this->_constants)) {
-				$fromUnit = $this->_constants[$this->_fromUnit];
-				$toUnit = $this->_constants[$toUnit];
-				return ($this->_value * $fromUnit) / $toUnit;
-			} else {
-				throw new \Exception("{toUnit} is not a supported unit", 1);
-				
-			}
-
+				$fromUnit = $this->fetchUnit($this->_fromUnit);
+				$toUnit = $this->fetchUnit($toUnit);
+				$toValue = ($this->_fromValue * $fromUnit) / $toUnit;
+				return $toValue;
 		}
 
+		/**
+		 * Retrieve the required conversion factor for the required unit
+		 * @param string $unit The unit to be retrieved
+		 * @return float 	   The conversion factor for the given unit
+		 * @throws \Exception  The required unit is not registered
+		 */
+		protected function fetchUnit($unit) {
+
+			foreach ($this->_units as $key => $value) {
+				if ($unit === $key || $this->isAlias($unit, $key)) {
+					return $value;
+				}
+			}
+
+			throw new \Exception("{$unit} is not a supported unit", 1);
+		}
+
+		/**
+		 * Check if the required unit is an alias of a registered unit
+		 * @param string $unit The required unit to be tested
+		 * @param string $key  The array of the registered unit to be tested
+		 * @return boolean
+		 */
+		protected function isAlias($unit, $key) {
+
+			return in_array($unit, $this->_aliases[$key]);
+		}
+
+		/**
+		 * Magic method allowing any unit names to be used to call conversion
+		 * @param string $toUnit The desired unit of measure
+		 * @return float 		 Converted measurement from callback
+		 */
 		public function __call($toUnit, $parameters){
 
 			return call_user_func(array($this, 'lengthConversion'), $toUnit);
