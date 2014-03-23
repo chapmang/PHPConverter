a940c3c9227f4177ff7509c581759ff1ceb50778
chapmang <geoff.chapman@mac.com>
2014-03-23 21:37:51 +0000

    Tweaks to area

--- converter/area.php
+++ converter/area.php
@@ -4,30 +4,42 @@ namespace PHPConverter\Converter {
 	use PHPConverter\Core\Configuration as Configuration;
 
 	/**
-	* 
-	*/
+	 * Area
+	 * Converter for converting between units of area
+	 * @author Geoff Chapman <geoff.chapman@mc.com>
+	 * @version 1.0
+	 * @package PHPConverter\Converter 
+	 */
 	class Area extends Converter {
 		
-		protected $_constants = array();
-
+		/**
+		 * Retrieve the preregistered units and aliases for this measurement
+		 * @return void
+		 */
 		public function __construct() {
 
-			$this->_constants = Configuration::get('area');
+			$this->_units = Configuration::get('area.units');
+			$this->_aliases = Configuration::get('area.aliases');
 		}
 
+		/**
+		 * Convert measurement to the given unit
+		 * @param string $toUnit Unit to be converted to
+		 * @return float 		 The measurement in the desired unit
+		 */
 		protected function areaConversion($toUnit) {
 
-			if (array_key_exists($this->_fromUnit, $this->_constants)) {
-				$fromUnit = $this->_constants[$this->_fromUnit];
-				$toUnit = $this->_constants[$toUnit];
-				return ($this->_value * $fromUnit) / $toUnit;
-			} else {
-				throw new \Exception("{toUnit} is not a supported unit", 1);
-				
-			}
-
+				$fromUnit = $this->fetchUnit($this->_fromUnit);
+				$toUnit = $this->fetchUnit($toUnit);
+				$toValue = ($this->_fromValue * $fromUnit) / $toUnit;
+				return $toValue;
 		}
 
+		/**
+		 * Magic method allowing all unit names to be used to call the conversion
+		 * @param string $toUnit The desired unit of measure
+		 * @return float 		 Converted measurement from callback
+		 */
 		public function __call($toUnit, $parameters){
 
 			return call_user_func(array($this, 'areaConversion'), $toUnit);
